c7f6dc2ac008f2a9e02db032906b9ccb5269cf85
Oliver Kaminski <ljurtgtffg@gmail.com>
2017-04-30 21:21:23 +0200

    Not working

--- public/src/pages/login/login.component.js
+++ public/src/pages/login/login.component.js
@@ -1,12 +1,37 @@
 import {Component} from '@angular/core';
+import template from './login.component.html';
+import './login.scss';
+import {AngularFire, AuthMethods, AuthProviders} from 'angularfire2';
 
 export class LoginComponent {
 
+    constructor(af){
+        this.af = af;
+    }
+    login(){
+        console.log(this.email);
+        this.af.auth.login({
+            email: this.email,
+            password: this.haslo
+        }, {
+            method: AuthMethods.Password
+        }).catch(()=>{
+            this.af.auth.createUser({
+                email: this.email,
+                password: this.haslo
+            })
+        });
+
+    }
 }
 
 LoginComponent.annotations = [
     new Component({
         selector: 'login',
-        template: `login`
+        template: template
     })
+];
+
+LoginComponent.parameters = [
+    [AngularFire]
 ];
--- public/src/player/player.component.js
+++ public/src/player/player.component.js
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import template from './player.component.html';
 import {PointsSvc} from '../services/pointsSvc';
+import {AngularFire} from 'angularfire2';
 import './player.scss';
 
 export class PlayerComponent {
-    constructor(pointsSvc){
-        this.name = 'Oliver';
+    constructor(pointsSvc, af){
+        af.auth.subscribe(user =>{
+            console.log(user)
+            this.name = user.auth.email;
+        });
+        // this.name = 'Oliver';
         this.obj = pointsSvc.obj;
     }
 
@@ -22,5 +27,6 @@ PlayerComponent.annotations = [
     })
 ];
 PlayerComponent.parameters = [
-    [PointsSvc]
+    [PointsSvc],
+    [AngularFire]
 ];
